b13a341c4e89525eebb77f45e342481f230d7a52
Tiago Ribeiro <tiagoribeirodev@gmail.com>
2026-02-11 00:47:57 -0300

    fix: update streamlit syntax and requirements

---
+++ test2.py
@@ -0,0 +1,5 @@
+import streamlit as st
+st.set_page_config(layout="wide")
+st.title("🚀 TESTE DE ATUALIZAÇÃO")
+st.success("App atualizado em: " + st.__version__)
+st.write("Se você vê esta mensagem, o app está atualizado!")
